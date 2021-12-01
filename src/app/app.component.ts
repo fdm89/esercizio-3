9247568919c1b9b5478f5ae58195738a6e1de5f7
[fdm89] <[fulvio.dimaio1@gmail.com]>
2021-12-01 16:43:34 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eserciziotre-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'eserciziotre';
+}
